6bc9479c76fe52737bdc030588478d070fe4e62e
ukdgad <david_gadsby@waters.com>
2021-12-04 06:34:47 +0000

    create basic plot

--- projects/data-viewer/src/lib/data-viewer.component.ts
+++ projects/data-viewer/src/lib/data-viewer.component.ts
@@ -1,20 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from "graphql-tag";
+import { map } from 'rxjs';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'lib-data-viewer',
   template: `
-    <p>
-      data-viewer works!
-    </p>
+   <div>
+    <canvas #chart></canvas>
+    </div>
   `,
   styles: [
   ]
 })
 export class DataViewerComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('chart', { static: true }) chartElement?: ElementRef;
+  chart!: Chart;
+
+  constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
+
+    this.createPlot();
+
+    this.apollo
+      .watchQuery({
+        query: gql`
+{
+  countries(names: ["United Kingdom"]) {
+    name
+    results {
+      date
+      label: date
+      confirmed
+    }
+  }
+}
+`,
+      })
+      .valueChanges.pipe(map((payload: any) => payload.data.countries)).subscribe((countries: any) => {
+        console.log('got data', countries);
+
+        const dataSeries = {
+          data: countries[0].results.map((r: any) => {
+            return {
+              x: r.date,
+              y: r.confirmed
+            };
+          }),
+          label: countries[0].name,
+          borderColor: '#00c4b6',
+          borderWidth: 0.5,
+          pointBackgroundColor: '#00c4b6',
+          showLine: false,
+        }
+          this.chart.data.datasets = [dataSeries];
+          this.chart.update();
+      })
+  }
+
+  private createPlot() {
+    console.log('creating plot');
+    Chart.defaults.global.defaultFontColor = '#AAA';
+
+    let ctx = this.chartElement?.nativeElement;
+    ctx = ctx.getContext('2d');
+    this.chart = new Chart(ctx, {
+      type: 'scatter',
+      data: {
+        datasets: []
+      },
+
+      // options for all datasets
+      options: {
+        maintainAspectRatio: true,
+        responsive: true,
+        scales: {
+          xAxes: [{
+            type: 'time'
+          }]
+        }
+      }
+    });
   }
 
 }
--- projects/data-viewer/src/lib/data-viewer.module.ts
+++ projects/data-viewer/src/lib/data-viewer.module.ts
@@ -1,6 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataViewerComponent } from './data-viewer.component';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
+import { HttpClientModule } from '@angular/common/http';
+
+const uri = 'https://covid19-graphql.now.sh/'; // <-- add the URL of the GraphQL server here
+export function createApollo(httpLink: HttpLink) {
+  return {
+    link: httpLink.create({uri}),
+    cache: new InMemoryCache(),
+  };
+}
 
 const routes: Routes = [
   {
@@ -13,11 +25,20 @@ const routes: Routes = [
   declarations: [
     DataViewerComponent
   ],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
   imports: [
     RouterModule.forChild(routes),
+    HttpLinkModule, ApolloModule, HttpClientModule
+
   ],
   exports: [
-    DataViewerComponent
+    DataViewerComponent, ApolloModule
   ]
 })
 export class DataViewerModule { }
