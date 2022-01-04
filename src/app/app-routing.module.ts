0622d17e43aab98232c82162e2118105189da418
ukdgad <david_gadsby@waters.com>
2022-01-04 14:52:10 +0000

    Add comments to app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 const routes: Routes = [
-  {
-    path: 'country',
-    pathMatch: 'prefix',
-    loadChildren: () => import('../../projects/country/src/lib/country.module').then(m => m.CountryModule),
-  },
+  // Dashboard MFE 
   {
     path: 'dashboard',
     pathMatch: 'prefix',
     loadChildren: () => import('../../projects/dashboard/src/lib/dashboard.module').then(m => m.DashboardModule),
   },
-
+  /**
+   * Country MFE
+   * @param {string} country Name of country to view
+   * @param {string} period  Time period to view
+   */
+  {
+    path: 'country',
+    pathMatch: 'prefix',
+    loadChildren: () => import('../../projects/country/src/lib/country.module').then(m => m.CountryModule),
+  },
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
